cc0bd4352866c085eb5962862d19f2084c3c3f2c
spolex <spolex@msn.com>
2019-09-01 16:07:06 -0400

    working with lda and debugging feature extraction functions

---
+++ test/test_arq_features.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 08:25:59 2019
+
+@author: isancmen
+"""
+
+from feature_extract.arq_features import log_detector
+import numpy as np
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_len(self):
+        l = np.arange(1, 11)
+        self.assertTrue(l.all() == np.arange(1, 11).all())
+
+    def test_lenn(self):
+        l = np.arange(1, 11)
+        self.assertTrue(len(l) == 10)
+        self.assertFalse(len(l) > 11)
+
+    def test_logdetector(self):
+        l = np.arange(2, 12)
+        # 6.3387402669680695
+        self.assertTrue(log_detector(l) > 6.)
+        self.assertFalse(log_detector(l) > 7.)
+        l[-1] = -11
+        # 20.763409211116233
+        assertTrue(log_detector(l) > 20.)
+        self.assertFalse(log_detector(l) > 21.)
